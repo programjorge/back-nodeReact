7861b86909dd5562f501b7b11f5982dadfcffd36
jorge_daw <solaboxgamer@gmail.com>
2023-05-14 19:24:11 +0200

    second commit

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,12 +2,11 @@ module.exports = app => {
   const Users = require("../controllers/User.controller.js");
 
   var router = require("express").Router();
-console.log("test")
   // Create a new Tutorial
   // router.post("/", tutorials.create);
 
   // Retrieve all Tutorials
-  router.get("/", Users.findAll);
+  router.get("/", Users.findAll)
 
   // // Retrieve all published Tutorials
   // router.get("/published", tutorials.findAllPublished);
