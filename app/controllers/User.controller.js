58eeba5565627de4084c379e8dc7ea086e7db906
jorge_daw <solaboxgamer@gmail.com>
2023-05-16 21:11:47 +0200

    first adding post register

--- app/controllers/User.controller.js
+++ app/controllers/User.controller.js
@@ -3,33 +3,33 @@ const User = db.user;
 const Op = db.Sequelize.Op;
 
 // Create and Save a new Tutorial
-// exports.create = (req, res) => {
+exports.create = (req, res) => {
   // Validate request
-  // if (!req.body.title) {
-  //   res.status(400).send({
-  //     message: "Content can not be empty!"
-  //   });
-  //   return;
-  // }
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
 
   // Create a Tutorial
-//   const User = {
-//     userName: req.body.userName,
-//     Password: req.body.Password
-//   };
+  const User = {
+    userName: req.body.userName,
+    Password: req.body.Password
+  };
 
-//   // Save Tutorial in the database
-//   Tutorial.create(tutorial)
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || "Some error occurred while creating the Tutorial."
-//       });
-//     });
-// };
+  // Save Tutorial in the database
+  User.create(User)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
+};
 
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
